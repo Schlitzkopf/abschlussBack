7f8474a578432d45fa41ac41af919001143c01d2
Ferdinand <holdenriederferdinand@gmail.com>
2022-01-26 12:35:19 +0100

    nice to have fehlt noch

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import "dotenv/config.js";
 import express from "express";
+import cors from "cors";
+import fileUpload from "express-fileupload"
+import bodyParser from "body-parser";
+import morgan from "morgan";
 import "./db/client.js";
 import animal from "./routes/animals.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(fileUpload({
+    createParentPath: true
+}));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
 
 app.use(express.json());
 app.use("/animal", animal);
