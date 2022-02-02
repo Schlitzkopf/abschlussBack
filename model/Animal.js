c382ec5db01313d8b34ffc95af4ce5ffaa563189
Ferdinand <holdenriederferdinand@gmail.com>
2022-02-02 14:58:10 +0100

    birth zu geburtstag

--- model/Animal.js
+++ model/Animal.js
@@ -4,20 +4,20 @@ const { Schema, model } = mongoose;
 const animalSchema = new Schema({
   name: { type: String, required: true },
   rasse: { type: String, required: true },
+  gewicht: {type: String},
   geschlecht: { type: String, required: true },
-  kastriert: { type: String, required: true },
-  verhalten: { type: String },
-  vertraeglickeit: { type: String },
-  birth: { type: Number, required: true },
   gechipt: { type: String, required: true },
   geimpft: { type: String, required: true },
+  kastriert: { type: String, required: true },
   kinderfreundlich: { type: String },
+  vertraeglickeit: { type: String },
+  geburtstag: { type: Number, required: true },
   ort: { type: String, required: true },
   bildUrl: { type: String },
-  videoUrl: { type: String },
   tier: { type: String, required: true },
   beschreibung: { type: String },
-  gewicht: {type: String}
+  videoUrl: { type: String },
+  verhalten: { type: String }
 });
 
 export default model("Animal", animalSchema);
